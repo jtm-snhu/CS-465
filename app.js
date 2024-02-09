b4ccb5d8151ef4f598995367080bad119903015f
Jeff McGinty <jfooze@gmail.com>
2024-02-09 17:18:40 -0300

    Created basic application API

--- app.js
+++ app.js
@@ -8,11 +8,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const hbs = require('hbs');
+require('./app_api/models/db');
 
-var indexRouter = require('./app_server/routes/index');
-var usersRouter = require('./app_server/routes/users');
-var travelRouter = require('./app_server/routes/travel');
-var roomsRouter = require('./app_server/routes/rooms');
+const indexRouter = require('./app_server/routes/index');
+const usersRouter = require('./app_server/routes/users');
+const travelRouter = require('./app_server/routes/travel');
+const roomsRouter = require('./app_server/routes/rooms');
+const apiRouter = require('./app_api/routes/index');
+
 var app = express();
 
 // view engine setup
@@ -32,8 +35,9 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
 app.use('/rooms', roomsRouter);
+app.use('/api', apiRouter);
 
-//Moved from above so it is declared after other routesb
+//Moved from above so it is declared after other routes
 app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
